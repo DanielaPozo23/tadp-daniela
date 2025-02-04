e8753afadc44bca08728af40bcd92d69bed61f75
DanielaPozo23 <garciapozoerikadaniela@gmail.com>
2025-02-03 18:07:29 -0600

    Agregue una tarjeta

--- src/components/productos/ItemProductos/ItemProductos.js
+++ src/components/productos/ItemProductos/ItemProductos.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-export function ItemProductos({producto}) {
+export function ItemProductos({ producto }) {
   return (
-    <div>
-      <h2>Nombre: {producto.nombre}</h2>
-      <h2>Precio: {producto.precio}</h2>
-      <h2>Descripción: {producto.unidad}</h2>
-      <h2>Contenido: {producto.contenido}</h2>
-      <img
-      src={producto.imagen}
-      style={{ width: "150px", height: "150px", borderRadius: "10px" }} 
-      />
+    <div className="d-flex flex-row flex-wrap gap-3 justify-content-center">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={producto.imagen} />
+        <Card.Body>
+          <Card.Title>{producto.nombre}</Card.Title>
+          <Card.Text>
+            <h2>Precio: {producto.precio}</h2>
+            <h2>Descripción: {producto.unidad}</h2>
+            <h2>Contenido: {producto.contenido}</h2>
+          </Card.Text>
+          <Button variant="primary">Comprar</Button>
+        </Card.Body>
+      </Card>
     </div>
-  )
+  );
 }
-
