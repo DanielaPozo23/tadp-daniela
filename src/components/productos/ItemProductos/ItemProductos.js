8ca7ebdc561c32339aa94dcc3b5403789201a55a
DanielaPozo23 <garciapozoerikadaniela@gmail.com>
2025-02-08 22:02:17 -0600

    Actualizacion de tarjetas

--- src/components/productos/ItemProductos/ItemProductos.js
+++ src/components/productos/ItemProductos/ItemProductos.js
@@ -1,22 +1,20 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Card } from "react-bootstrap";
 
-export function ItemProductos({ producto }) {
+export function ItemProductos({producto}) {
   return (
-    <div className="d-flex flex-row flex-wrap gap-3 justify-content-center">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={producto.imagen} />
+    
+      <Card>
+        <Card.Img variant="top" src={producto.imagen} style={{width:"200px", height:"70%"}} />
         <Card.Body>
-          <Card.Title>{producto.nombre}</Card.Title>
+          <Card.Title>Nombre: {producto.nombre}</Card.Title>
           <Card.Text>
-            <h2>Precio: {producto.precio}</h2>
-            <h2>Descripción: {producto.unidad}</h2>
-            <h2>Contenido: {producto.contenido}</h2>
+           <p>Precio: {producto.precio}</p>
+            <p>Unidades: {producto.unidad}</p>
+            <p>Contenido neto: {producto.contenido}</p>
           </Card.Text>
-          <Button variant="primary">Comprar</Button>
         </Card.Body>
       </Card>
-    </div>
+  
   );
 }
